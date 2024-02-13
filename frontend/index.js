cbaedad16a744536f92f56a9327eca6f779fccbe
Haskely <Haskely@live.com>
2024-02-13 10:46:58 +0800

    format

--- frontend/index.js
+++ frontend/index.js
@@ -1,37 +1,37 @@
-document.addEventListener('alpine:init', () => {
-    Alpine.data('dropdown', () => ({
-        open: false,
+document.addEventListener("alpine:init", () => {
+  Alpine.data("dropdown", () => ({
+    open: false,
 
-        toggle() {
-            this.open = ! this.open
-        },
-    }));
+    toggle() {
+      this.open = !this.open;
+    },
+  }));
 
-    Alpine.data('timer', () => ({
-        timer: null,
-        counter: 0,
-        init() {
-          // Register an event handler that references the component instance
-          this.timer = setInterval(() => {
-            // console.log('Increased counter to', ++this.counter);
-            ++this.counter;
-          }, 1000);
-        },
-        destroy() {
-            // Detach the handler, avoiding memory and side-effect leakage
-            clearInterval(this.timer);
-        },
-    }));
+  Alpine.data("timer", () => ({
+    timer: null,
+    counter: 0,
+    init() {
+      // Register an event handler that references the component instance
+      this.timer = setInterval(() => {
+        // console.log('Increased counter to', ++this.counter);
+        ++this.counter;
+      }, 1000);
+    },
+    destroy() {
+      // Detach the handler, avoiding memory and side-effect leakage
+      clearInterval(this.timer);
+    },
+  }));
 
-    Alpine.store('darkMode', {
-        init() {
-            this.on = window.matchMedia('(prefers-color-scheme: dark)').matches
-        },
+  Alpine.store("darkMode", {
+    init() {
+      this.on = window.matchMedia("(prefers-color-scheme: dark)").matches;
+    },
 
-        on: false,
+    on: false,
 
-        toggle() {
-            this.on = ! this.on
-        }
-    })
-})
+    toggle() {
+      this.on = !this.on;
+    },
+  });
+});
